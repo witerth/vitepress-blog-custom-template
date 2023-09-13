9920f3a1c60f51708dce45d057c9da7264a6d9bd
Renn <906155099@qq.com>
2023-09-14 00:10:11 +0800

    feat: post layout 使用自定义大纲组件

--- src/layout/Post.vue
+++ src/layout/Post.vue
@@ -5,7 +5,8 @@ import PageMeta from "../components/PageMeta.vue";
 import { getPage } from "../utils";
 import MainContent from "../components/MainContent";
 import CommentList from "../components/CommentList";
-
+import DocOutline from "../components/DocOutline/DocOutline.vue";
+import { Page } from "@/types";
 const route = useRoute();
 
 const pageName = computed(() =>
@@ -13,7 +14,7 @@ const pageName = computed(() =>
 );
 
 const page = computed(() => {
-  return getPage(route.path);
+  return getPage(route.path) as Page;
 });
 
 const { site } = useData();
@@ -31,36 +32,67 @@ const showComment = () => {
 </script>
 
 <template>
-  <MainContent>
-    <header class="post-title text-center">
-      <h1>{{ page.title }}</h1>
-    </header>
-    <PageMeta :show-edit-link="true" :page="page" />
-    <Content
-      class="post-content vp-doc prose dark:prose-invert"
-      :class="pageName"
-    />
-    <footer
-      class="post-content"
-      v-if="page.frontmatter.layout == 'issue' && showComment()"
-    >
-      <CommentList
-        :id="page.frontmatter.id"
-        :editUrl="page.frontmatter.editLink"
+  <div class="post-container center">
+    <MainContent>
+      <header class="post-title text-center">
+        <h1>{{ page?.title }}</h1>
+      </header>
+      <PageMeta :show-edit-link="true" :page="page" />
+      <Content
+        class="post-content vp-doc dark:prose-invert"
+        :class="pageName"
       />
-    </footer>
-  </MainContent>
+      <footer
+        class="post-content"
+        v-if="page?.frontmatter.layout == 'issue' && showComment()"
+      >
+        <CommentList
+          :id="page.frontmatter.id"
+          :editUrl="page.frontmatter.editLink"
+        />
+      </footer>
+    </MainContent>
+    <DocOutline></DocOutline>
+  </div>
 </template>
 
 <style scoped>
-@media screen and (min-width: 600px) {
-  .post-content {
-    @apply max-w-[900px] min-w-[700px] xl:mr-20 xl:ml-20;
+.post-container {
+  width: 100%;
+  padding: 30px 20px 0;
+}
+.main-content-center {
+  display: flex;
+  justify-content: center;
+  padding: 30px 40px;
+  width: calc(100% - 252px);
+  /* background: #fff !important; */
+  background-color: var(--vp-nav-bg-color);
+  font-family: -apple-system,system-ui,BlinkMacSystemFont,Helvetica Neue,PingFang SC,Hiragino Sans GB,Microsoft YaHei,Arial,sans-serif;
+    background-image: linear-gradient(90deg,rgba(159,219,252,.15) 3%,transparent 0),linear-gradient(1turn,rgba(159,219,252,.15) 3%,transparent 0);
+    background-size: 20px 20px;
+    background-position: 50%;
+
+}
+.main-content-solot {
+  width: 100%;
+}
+@media screen and (min-width: 768px) {
+  .post-container {
+    @apply max-w-[1080px] min-w-[700px] xl:mr-20 xl:ml-20;
+  }
+
+}
+@media screen and (max-width: 768px) {
+
+  .main-content-center {
+  width: 100%;
+
   }
 }
 
 @media (min-width: 1480px) {
-  .post-content {
+  .post-container {
     min-width: 640px;
   }
 }
